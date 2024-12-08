2ff388b82fd1331c12ac104e5eca3678714e3410
chocoanish <chocoboyanish566@gmail.com>
2024-12-08 11:27:21 +0530

    Landing Page Almost Completed

--- yugaantar_beta/src/assets/Images/index.tsx
+++
@@ -1,4 +0,0 @@
-export { default as festTitle} from './Title.svg';
-export { default as primeEvent1 } from './GamingArena.png'
-export { default as primeEvent2 } from './MusicRevolution.png'
-export { default as primeEvent3 } from './TechOdyssey.png'
